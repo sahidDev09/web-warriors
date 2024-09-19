b1f82c909eacca325de5e4a5e651d69d75440dfe
sahidDev09 <iamsahidofficial99@gmail.com>
2024-09-19 08:33:10 +0600

    create new ul tag

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <>
       <h1>hello world</h1>
+      <ul>
+        <li>abhid</li>
+        <li>sahid</li>
+        <li>mumu</li>
+        <li>rose</li>
+        <li>rahra</li>
+      </ul>
     </>
   );
 }
