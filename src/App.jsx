442b7b2cd7442e199a4a6cb10afdd44936329747
sahidDev09 <iamsahidofficial99@gmail.com>
2024-09-19 08:29:07 +0600

    created new de1 branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <h1>hello world</h1>
+      <Login />
     </>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+
+
+const Login = () => {
+  return (
+    <div>
+      <form action="">
+        <input placeholder="enter your name" type="text" name="" id="" />
+      </form>
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default Login;
